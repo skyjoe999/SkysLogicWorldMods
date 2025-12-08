fdaf90d7f76959713c01912292d7238f1624af43
Joseph Bell <joe@onmyhonor.com>
2025-12-08 17:49:55 -0500

    Initial Upload
    A small tweak that allows you to customize where things can be placed on a board.

---
+++ SkysCircuitBoardPlacementWorkaround/SkysCircuitBoardPlacementWorkaround/src/client/ClientMod.cs
@@ -0,0 +1,35 @@
+using HarmonyLib;
+using LogicAPI.Client;
+using LogicAPI.Data;
+using LogicWorld.Building.Overhaul;
+using LogicWorld.Interfaces;
+using LogicWorld.Physics;
+
+namespace SkysCircuitBoardPlacementWorkaround.Client;
+
+public class SkysCircuitBoardPlacementWorkaround_ClientMod : ClientMod
+{
+    protected override void Initialize()
+    {
+        var harmony = new Harmony("SkysCircuitBoardPlacementWorkaroundClient");
+        // Harmony.DEBUG = true;
+        harmony.PatchAll();
+    }
+}
+// ReSharper disable All
+[HarmonyPatch(typeof(StuffPlacer), nameof(StuffPlacer.CanMoveOn))]
+class Patch
+{
+
+    static bool Prefix(HitInfo info, out bool __result)
+    {
+        __result = false;
+        if (info.cAddress == ComponentAddress.Empty)
+            return true;
+        IComponentClientCode clientCode = Instances.MainWorld.Renderer.Entities.GetClientCode(info.cAddress);
+        if (clientCode is not SemiCircuitBoard)
+            return true;
+        __result = (clientCode as SemiCircuitBoard).CanMoveOn(info);
+        return false;
+    }
+}
---
+++ SkysCircuitBoardPlacementWorkaround/SkysCircuitBoardPlacementWorkaround/src/client/SemiCircuitBoard.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Reflection;
+using LogicAPI.Data.BuildingRequests;
+using LogicWorld.BuildingManagement;
+using LogicWorld.ClientCode;
+using LogicWorld.Interfaces;
+using LogicWorld.Physics;
+using LogicWorld.Rendering.Components;
+using LogicWorld.SharedCode.BinaryStuff;
+using SkysGeneralLib.Shared;
+
+namespace SkysCircuitBoardPlacementWorkaround.Client;
+
+public abstract class SemiCircuitBoard : CircuitBoard
+{
+    public abstract bool CanMoveOn(HitInfo info);
+}
+
+// Would love to make this a generic pattern except I cant without c++ style templates
+public abstract class SemiCircuitBoard<TData> : 
+    SemiCircuitBoard, IComponentClientCode where TData : class, CircuitBoard.IData
+{
+    private readonly CustomDataManager<TData> DataManager;
+
+    protected SemiCircuitBoard()
+    {
+        DataManager = new CustomDataManager<TData>();
+        typeof(ComponentClientCode<IData>)
+            .GetField("DataManager", BindingFlags.Instance | BindingFlags.NonPublic)!
+            .SetValue(this, new CustomDataManagerDownCast<TData, IData>(DataManager));
+        DataManager.OnPropertySet += (Action)(() =>
+        {
+            var numArray = SerializeCustomData();
+            LoadCustomDataClientSide(numArray);
+            if (!PlacedInMainWorld)
+                return;
+            BuildRequestManager.SendBuildRequestWithoutAddingToUndoStack(
+                new BuildRequest_UpdateComponentCustomData(Address, numArray));
+        });
+    }
+
+    public new TData Data => DataManager.Data;
+
+    object IComponentClientCode.CustomDataObject => Data;
+    public override byte[] SerializeCustomData() => DataManager.SerializeData();
+
+    protected override void DeserializeData(byte[] data)
+    {
+        if (data == null)
+            SetDefaultCustomData();
+        else if (!DataManager.TryDeserializeData(data))
+            throw new Exception(
+                $"Error deserializing data for component at {Address} of type {Component?.Data.Type}");
+    }
+
+    private void SetDefaultCustomData() => SetDataDefaultValues();
+
+    protected abstract override void SetDataDefaultValues();
+
+    private void LoadCustomDataClientSide(byte[] data, bool queueDataUpdate = true)
+    {
+        DeserializeData(data);
+        if (!queueDataUpdate)
+            return;
+        QueueDataUpdate();
+    }
+}
